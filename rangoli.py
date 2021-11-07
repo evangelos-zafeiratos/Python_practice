3a04419d1dd8ac1f9b9e70a5a9d4fdf1b0e92520
Evangelos Zafeiratos <74437675+evangelos-zafeiratos@users.noreply.github.com>
2021-11-08 00:35:35 +0100

    Update rangoli.py

--- rangoli.py
+++ rangoli.py
@@ -1,11 +1,32 @@
 def print_rangoli(size):
     # your code goes here
+    lines_list = list()
     alphabet_index = size - 1
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    dash_number = (size-1) * 2
-    for i in range(size):
-        print('-'*dash_number)
-        
+    reversed_alphabet = 'zyxwvutsrqponmlkjihgfedcba'
+    dashes = (size - 1) * 2
+    alphabet_index = size - 1
+
+    for line_no in range(size):
+        line = ''
+        sliced_alphabet = reversed_alphabet[26 - size:]
+        line = line + '-' * dashes
+        #print('-'* dashes, end='')
+        for j in range(line_no):
+            line = line + sliced_alphabet[j] + '-'
+        line = line + sliced_alphabet[line_no]
+        dashes = dashes - 2
+
+        reversed_line = ''
+        for index in range(len(line) - 1):  # Create a reverse line string including all the characters except for the last which does not need to be repeated
+            reversed_line = line[index] + reversed_line
+        lines_list.append(line+reversed_line) # Merge the line & reversed version and send them to the list
+    for line in lines_list:
+        print(line)
+    del lines_list[-1]
+    for line in reversed(lines_list):
+        print(line)
+
+
 
 if __name__ == '__main__':
     n = int(input())
